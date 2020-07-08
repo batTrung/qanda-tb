1c439b8498a3850dc8f7871a228e9689aeff8754
batTrung <trungbat.pv@gmail.com>
2020-07-08 21:54:02 +0700

    split settings file

---
+++ pyforum/config/local_settings.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+import logging
+
+from .settings import *  # noqa
+
+
+# ==============================================================================
+# CORE SETTINGS
+# ==============================================================================
+
+INSTALLED_APPS += [  # noqa
+    'debug_toolbar',
+]
+
+
+# ==============================================================================
+# MIDDLEWARE SETTINGS
+# ==============================================================================
+
+DEBUG_MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+]
+
+MIDDLEWARE = DEBUG_MIDDLEWARE + MIDDLEWARE   # noqa
